13fe85b103f871e0de9a9c691d73aa6f6b51a332
falexakos <falexakos@sch.gr>
2017-06-14 10:35:22 +0300

    First commit
    main() routine here

---
+++ extractf.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+
+#ifndef DEFWIDTH
+ #define DEFWIDTH 176
+#endif
+#ifndef DEFHEIGHT
+ #define DEFHEIGHT 144
+#endif
+
+int main(int argc, char *argv[])
+{
+
+	if (argc < 2)
+	{
+		fprintf(stderr, "Syntax: %s [Width] [Height] [-nNumber] filename\n", argv[0]);
+		fprintf(stderr, "To extract <number> frames of raw video with resolution of <Width x Height>\n");
+	}
+
+
+	return 0;
+}
+
